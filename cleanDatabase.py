323d5e9e48e29ff6bfd5cad2b546fec7f058d5d7
Jenny Müller <jenny.mul@outlook.com>
2024-03-11 13:59:59 +0100

    Create cleanDasebase file

---
+++ cleanDatabase.py
@@ -0,0 +1,9 @@
+import os 
+from createDatabase import createDatabase
+from fillDatabase import fillDatabase
+
+if os.path.exists("trondelagTeater.db"):
+    os.remove("trondelagTeater.db")
+
+createDatabase()
+fillDatabase()
